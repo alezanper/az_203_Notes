0d1f4b38916bd089a479594e4f1f63987af53ed8
Alexander Benavides <fabenavideso@gmail.com>
2020-02-02 00:07:40 -0500

    updating files

--- provision VMs/Create_VM.ps1
+++ provision VMs/Create_VM.ps1
@@ -1,48 +1,48 @@
-#"Logged into azure powershell, with connect-AzureRmAxxount"
-
-#Install AzureRm.Netcore in PowerShell Core
-sudo PowerShell
+# Install AzureRm.Netcore in PowerShell Core
 Install-Module AzureRm.Netcore
 
-exit
+# Start a connection with Azure
+Connect-AzureRmAccount 
 
-#Start a connection with Azure
-Connect-AzureRmAccount -Subscription 'Demostration Account'
+# For creating a new resourcegroup
+New-AzureRmResourceGroup `
+   -Name 'myrg' `
+   -Location 'Centralus'
 
-#Create a Linux VM 
+# For getting the resourcegroup
 $rg = Get-AzureRmResourceGroup `
-    -Name 'myrg'
+    -Name 'myrg' `
     -Location 'Centralus'
 
 $rg
 
-#Create a subnet configuration
+# For Creating a subnet configuration
 $subnetConfig = New-AzureRmVirtualNetworkSubnetConfig `
-    -Name 'subnet-2' `
+    -Name 'subnet-1' `
     -AddressPrefix '10.2.1.0/24' `
 
 $subnetConfig
 
-#Create a virtual network
+# For Creating a virtual network
 $vnet = New-AzureRmVirtualNetwork `
     -ResourceGroupName $rg.ResourceGroupName `
-    -Location $rg.Location
-    -Name 'vnet-2' `
+    -Location $rg.Location `
+    -Name 'vnet-1' `
     -AddressPrefix '10.2.0.0/16' `
     -subnet $subnetConfig
 
 $vnet
 
-#Create an Ip
+# For creating a public Ip
 $pip = New-AzureRmPublicIpAddress `
     -ResourceGroupName $rg.ResourceGroupName `
     -Location $rg.Location `
-    -Name 'pip-2' `
+    -Name 'pip-1' `
     -AllocationMethod Static
 
 $pip
 
-#Create a networ security group rule for SSH
+# For Creating a network security group rule for SSH
 $rule1 = New-AzureRmNetworkSecurityRuleConfig `
     -Name ssh-rule `
     -Description 'Allow SSH' `
@@ -50,39 +50,44 @@ $rule1 = New-AzureRmNetworkSecurityRuleConfig `
     -Protocol Tcp `
     -Direction Inbound `
     -Priority 100 `
-    -SourceAddressPrefix Intenert `
+    -SourceAddressPrefix Internet `
     -SourcePortRange * `
     -DestinationAddressPrefix * `
     -DestinationPortRange 22
 
 $rule1
 
-#Create network security group with the rule
+# For Creating a network security group with the rule
 $nsg = New-AzureRmNetworkSecurityGroup `
     -ResourceGroupName $rg.ResourceGroupName `
     -Location $rg.Location `
-    -Name 'ngs-2' `
+    -Name 'ngs-1' `
     -SecurityRules $rule1
 
 $nsg | more
 
-#Create a virtual network card and associate with public IP address and NSG
-$subnet = $vnet.Subnets | Where-Object { $_.Name -eq 'subnet-2'}
+$vnet.Subnets
 
-$nic = New-AzureRmNetworkInterface
+# For Creating a virtual network card and associate with public IP address and NSG
+$subnet = $vnet.Subnets | Where-Object { $_.Name -eq 'subnet-1'}
+
+$subnet
+
+# For creating the network interface card
+$nic = New-AzureRmNetworkInterface `
     -ResourceGroupName $rg.ResourceGroupName `
     -Location $rg.Location `
-    -Name 'Nic-2' `
+    -Name 'Nic-1' `
     -Subnet $subnet `
     -PublicIpAddress $pip `
     -NetworkSecurityGroup $nsg
 
 $nic
 
-#Create a virtual machine configuration
+# For Creating the virtual machine configuration
 $LinuxVmConfig = New-AzureRmVMConfig `
-    -VMName 'linux-2'
-    -VMSize 'Standard-D1'
+    -VMName 'mylinux' `
+    -VMSize 'Standard_B1ms'
 
 $password = ConvertTo-SecureString 'myComplexPassword2020*' -AsPlainText -Force
 $LinuxCred = New-Object System.Management.Automation.PSCredential ('alezanper', $password)
@@ -90,18 +95,21 @@ $LinuxCred = New-Object System.Management.Automation.PSCredential ('alezanper', 
 $LinuxVmConfig = Set-AzureRmVMOperatingSystem `
     -VM $LinuxVmConfig `
     -Linux `
-    -ComputeName 'linux-2' `
-    -DisablePasswordAuthentication `
+    -ComputerName 'mylinux' `
+    #-DisablePasswordAuthentication `
     -Credential $LinuxCred
 
+# Optional adding SSH key
 $sshPublicKey = Get-Content "~/.ssh/id_rsa.pub"
 Add-AzureRmVMSshPublicKey `
     -VM $LinuxVmConfig `
     -KeyData $sshPublicKey `
     -Path "/home/alezanper/.ssh/authorized_keys"
 
+# Checking for images 
 Get-AzureRmVMImageSku -Location $rg.Location -PublisherName "Redhat" -Offer "rhel"
 
+# Configure the OS
 $LinuxVmConfig = Set-AzureRmVMSourceImage `
     -VM $LinuxVmConfig `
     -PublisherName 'Redhat' `
@@ -109,30 +117,18 @@ $LinuxVmConfig = Set-AzureRmVMSourceImage `
     -Skus '7.4' `
     -Version 'Latest'
 
+# Adding NIC
 $LinuxVmConfig = Add-AzureRmVMNetworkInterface `
-    -VM $LinuxVmConfig`
+    -VM $LinuxVmConfig `
     -Id $nic.Id
 
+# For creating the VM
 New-AzureRmVM `
     -ResourceGroupName $rg.ResourceGroupName `
     -Location $rg.Location `
     -VM $LinuxVmConfig
 
-$MyIp = Get-AzureRmPublicIpAddress `
-    -ResourceGroupName $rg.ResourceGroupName `
-    -Name $pip.Name | Select-Object -ExpandProperty AzureRmPublicIpAddress
+# Getting Ip address
+$publicIp = Get-AzureRmPublicIpAddress -Name $pip.Name -ResourceGroupName $rg.ResourceGroupName
 
-$MyIP
-
-ssh -l alezanper $MyIP
-
-
-
-
-
-
-
-
-
-
-
+$publicIp.IpAddress
